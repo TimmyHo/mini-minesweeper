f8b18d0b9e43e0a41c541b1127b718f4ba05dc38
Tim Hsiung <timothy.hsiung@gmail.com>
2017-03-10 13:40:57 +0800

    Initial blank project (not even run yet)

---
+++ app/src/main/java/com/timmyho/miniminesweeper/MinesweeperGame.java
@@ -0,0 +1,13 @@
+package com.timmyho.miniminesweeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MinesweeperGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minesweeper_game);
+    }
+}
