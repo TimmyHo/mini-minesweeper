11ae78c7c02616656b501468487e46f578fd7890
Tim Hsiung <timothy.hsiung@gmail.com>
2017-03-14 10:57:42 +0800

    Created a new SQLITE database and changed the page launched for testing purposes

--- app/src/main/java/com/timmyho/miniminesweeper/HighScoreList.java
+++ app/src/main/java/com/timmyho/miniminesweeper/HighScoreList.java
@@ -1,13 +1,23 @@
 package com.timmyho.miniminesweeper;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HighScoreList extends AppCompatActivity {
+    private SQLiteDatabase highScoreDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_score_list);
+
+        this.highScoreDB = openOrCreateDatabase("HighScoreDB", MODE_PRIVATE, null);
+
+        this.highScoreDB.execSQL(
+            "create table if not exists scores (" +
+                "id     INT        PRIMARY KEY   NOT NULL, " +
+                "name   VARCHAR(20)              NOT NULL, " +
+                "score  INT                      NOT NULL);");
     }
 }
