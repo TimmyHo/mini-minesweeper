e16da1c8ddced57de975f4ee2f2f261fc9fee7e7
Tim Hsiung <timothy.hsiung@gmail.com>
2017-03-14 12:18:54 +0800

    Generated random score data and also made a way to query it

--- app/src/main/java/com/timmyho/miniminesweeper/HighScoreList.java
+++ app/src/main/java/com/timmyho/miniminesweeper/HighScoreList.java
@@ -1,8 +1,10 @@
 package com.timmyho.miniminesweeper;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class HighScoreList extends AppCompatActivity {
     private SQLiteDatabase highScoreDB;
@@ -14,10 +16,27 @@ public class HighScoreList extends AppCompatActivity {
 
         this.highScoreDB = openOrCreateDatabase("HighScoreDB", MODE_PRIVATE, null);
 
-        this.highScoreDB.execSQL(
-            "create table if not exists scores (" +
-                "id     INT        PRIMARY KEY   NOT NULL, " +
+        // PROTO_ONLY this is just so I can fill the table up with random information to
+        // ensure it works
+        this.highScoreDB.execSQL("drop table if exists scoreList");
+        this.highScoreDB.execSQL("create table if not exists scoreList (" +
+                "id     INTEGER    PRIMARY KEY   AUTOINCREMENT, " +
                 "name   VARCHAR(20)              NOT NULL, " +
                 "score  INT                      NOT NULL);");
+
+        for (int i = 0; i < 10; i++) {
+            this.highScoreDB.execSQL(
+                "INSERT into scoreList (name, score) VALUES (\"Bob\", " + i + ");");
+        }
+        Cursor cr = this.highScoreDB.rawQuery("SELECT name, score from scoreList", null);
+
+        if (cr.moveToFirst()) {
+            do {
+                String name = cr.getString(cr.getColumnIndex("name"));
+                int score = cr.getInt(cr.getColumnIndex("score"));
+
+                Log.d("WriteDB", name+": "+score);
+            } while (cr.moveToNext());
+        }
     }
 }
