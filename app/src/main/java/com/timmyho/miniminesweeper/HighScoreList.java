296e225b1a3f5e8c4dbd97e0ff299fd18dcf9000
Tim Hsiung <timothy.hsiung@gmail.com>
2017-03-14 10:12:01 +0800

    Black page checkin for HighscoreList

---
+++ app/src/main/java/com/timmyho/miniminesweeper/HighScoreList.java
@@ -0,0 +1,13 @@
+package com.timmyho.miniminesweeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HighScoreList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_high_score_list);
+    }
+}
